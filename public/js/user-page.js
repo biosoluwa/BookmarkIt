af679c8ce73e0457355a9bc556b013682da5e150
biosoluwa <favbiose@gmail.com>
2026-05-25 00:49:45 +0100

    basic styling

--- public/js/user-page.js
+++ public/js/user-page.js
@@ -4,9 +4,17 @@ try{
 
     if(!res.ok){
         console.log(`Failed to fetch user's email: ${res.status}`)
-    }else{
-        console.log(data)
     }
-}catch(err){
-    console.error('Request error', err.message)
+
+ 
+    displayUserMail(data.email)
+    
+    }catch(err){
+        console.error('Request error', err.message)
+}
+
+function displayUserMail(mail){
+    const mailP = document.getElementById('user-mail')
+    mailP.innerHTML = mail
+
 }
