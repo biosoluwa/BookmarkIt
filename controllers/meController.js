7c3c0411c0d34f7d20d723ec763c9428ed263de6
biosoluwa <favbiose@gmail.com>
2026-05-27 00:38:20 +0100

    ....

--- controllers/meController.js
+++ controllers/meController.js
@@ -4,18 +4,11 @@ export async function meController(req, res){
 
     try{
 
-    const db = await dbConnection()
-
-    // if(!req.session.userId){
-    //     return res.json({
-    //         isLoggedin: false
-    //     })
-    // }
+    const db =  dbConnection()
 
 
-    const user = await db.get(`SELECT email FROM users WHERE id = ?`,
-        [req.session.userId]
-    );
+
+    const user =  db.prepare(`SELECT email FROM users WHERE id = ?`).get(req.session.userId);
 
     res.json({
         isLoggedin: true,
